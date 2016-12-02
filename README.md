b52670f6a33bf20e9a89e7d691f3f0dca26be969
zpkweb <547790132@qq.com>
2016-12-02 17:04:13 +0800

    demo init

--- README.md
+++ README.md
@@ -1,14 +1,44 @@
 # studycenterAppDemo
-### 一，搭建测试环境
+## 一，搭建测试环境
 工具：APICloud Studio, 系统：windows
-step 1:
-	创建项目测试目录
-	mkdir studycenterApp
-	下载代码到测试目录
-	git clone https://github.com/developercaicui/studycenterAppDemo.git
-step 2:
-	在APICloud Studio中,打开工作空间（测试目录），新建App项目studycenterAppDemo（跟github上面的目录一样）。
+######  step 1:
+
+创建项目测试目录
+	
+	'mkdir studycenterApp'	
+下载代码到测试目录
+	
+	'git clone git@github.com/developercaicui/studycenterAppDemo.git'(不要用https的方式)
+###### step 2:
+[Sublime3 WiFi真机同步和WiFi真机预览](http://docs.apicloud.com/Dev-Tools/sublime-wifi-sync)
 
 	
-step 3:
+######  step 3:
+安装库文件
 	
+	'npm install'
+开启代码检测
+
+	'gulp server'
+
+## 二，目录结构&提交代码
+> studycenterAppDemo：根目录
+	>> build：jade(html)模版&stylus(css)模版
+	>> demo：apicloud 的测试代码
+	>> node_modules：npm库文件
+
+获取远程dev分支
+	
+	'git checkout  –b dev origin/dev'
+进入demo目录，提交代码并添加提交信息
+	
+	'git add *'
+	'git commmit -m "提交信息"'
+返回根目录，上传到dev分支
+		
+	'git push origin dev'
+
+>多人协作工作模式一般是这样的：
+首先，可以试图用git push origin branch-name推送自己的修改.
+如果推送失败，则因为远程分支比你的本地更新早，需要先用git pull试图合并。
+如果合并有冲突，则需要解决冲突，并在本地提交。再用git push origin branch-name推送。
--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,25 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var stylus = require('gulp-stylus');
-var minifycss = require('gulp-minify-css');
-var htmlmin = require('gulp-htmlmin');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var del = require('del');
-var gulpif = require('gulp-if');
 var browserSync = require('browser-sync').create();
 
 gulp.task('server', function() {
     browserSync.init({
-        server: "./demo"
+        server: "./"
     });
-    gulp.watch("demo/build/jade/**/*.jade", ['jade']);
-    gulp.watch("demo/html/**").on('change', browserSync.reload);
+    gulp.watch("build/jade/**/*.jade", ['jade']);
+    // gulp.watch("html/**").on('change', browserSync.reload);
 
-    gulp.watch("demo/build/stylus/*.styl", ['stylus']);
-    gulp.watch("demo/css/**").on('change', browserSync.reload);
+    gulp.watch("build/stylus/*.styl", ['stylus']);
+    // gulp.watch("css/**").on('change', browserSync.reload);
 
-    gulp.watch("demo/script/**").on('change', browserSync.reload);
-    gulp.watch("*.html").on('change', browserSync.reload);
+    // gulp.watch("script/**").on('change', browserSync.reload);
+    // gulp.watch("*.html").on('change', browserSync.reload);
 });
 gulp.task('default', ['server']);
 
 gulp.task('jade', function() {
-  return gulp.src('demo/build/jade/template/*.jade')
+  return gulp.src('build/jade/template/*.jade')
     .pipe(jade({
       pretty: true
     }))
@@ -34,63 +27,7 @@ gulp.task('jade', function() {
 });
 
 gulp.task('stylus', function() {
-  return gulp.src('demo/build/stylus/*.styl')
+  return gulp.src('build/stylus/*.styl')
     .pipe(stylus())
     .pipe(gulp.dest('demo/css'));
 });
-
-
-gulp.task('minifyhtml', function() {
-  return gulp.src('demo/build/jade/template/*.jade')
-    .pipe(jade())
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-      removeComments: true,
-      removeSciptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true,
-      minifyJS: true,
-      minifyCSS: true
-    }))
-    .pipe(gulp.dest('app/studycenterApp/html'));
-});
-
-gulp.task('minifycss', ['stylus'], function() {
-  return gulp.src([
-    'demo/css/*.css',
-    ])
-    // .pipe(concat('studycenterapp.css'))
-    // .pipe(gulp.dest('app/css/'))
-    .pipe(minifycss())
-    // .pipe(rename({
-    //   suffix: '.min'
-    // }))
-    .pipe(gulp.dest('app/studycenterApp/css/'))
-});
-gulp.task('minifyjs', function() {
-  return gulp.src('demo/script/**\/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('app/studycenterApp/script'))
-});
-
-gulp.task('minifyLibs', function() {
-  return gulp.src([
-    'demo/[^bchs]**/**',
-    'demo/index.html',
-    'demo/config.xml'
-    ])
-    .pipe(gulpif(['*.html'],htmlmin({
-      collapseWhitespace: true,
-      removeComments: true,
-      removeSciptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true,
-      minifyJS: true,
-      minifyCSS: true
-    })))
-    .pipe(gulp.dest('app/studycenterApp'))
-});
-gulp.task('clean',function(){
-  //del(['app/**','!app']);
-})
-gulp.task('build', ['clean'], function(){
-  gulp.run(['minifyLibs','minifyhtml','minifycss','minifyjs',]);
-})
