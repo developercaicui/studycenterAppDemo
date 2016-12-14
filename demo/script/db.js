73c9eb6a6156ab14ca462d5fc1dbcc356fe3a9d7
zpkweb <547790132@qq.com>
2016-12-14 16:26:09 +0800

    db.js

--- demo/script/db.js
+++ demo/script/db.js
@@ -15,6 +15,7 @@
 	*		chapterName : '', // 章节名称
 	*		taskId : '', // 任务id
 	*		taskName ：'', // 任务名称
+	
 	*		progress : '', // 最后1次学习进度
 	*		total: '', // 任务总长度
 	*		state : '', // 是否播放结束
@@ -34,6 +35,7 @@
 	*	getTaskProgress(taskId, callback);  获取任务进度
 	*	getCourseTaskProgress(courseId, callback);  获取课程下面所有的任务进度
 	*	getTaskProgressNoSend(courseId, callback); 获取课程/所有未同步的任务进度
+	*	getTasksProgressSupplyAll(); 获取所有未发送的进度
 */
 ;(function(window){
 	var DB = {
@@ -186,6 +188,26 @@
 				}
 			});
 		},
+		getTasksProgressSupplyAll : function(){
+			DB.create(DB.taskNameDB,function(ret, err){
+				if(ret.status){
+					DB.isEmptyTasksProgress(function(isEmpty){
+						if(isEmpty){
+							var selectSql = 'SELECT progress FROM '+DB.taskNameTable+' where isSupply="1"';
+							DB.selectSql(DB.taskNameDB,selectSql,function(ret, err){
+								if(ret.status){
+									if(callback){callback(ret.data)}
+								}else{//
+									alert('获取失败');
+								}
+							})
+						}else{
+							alert('数据库为空');
+						}
+					})
+				}
+			});
+		}
 		saveTasksProgressSync : function(){ // 同步保存
 		},
 		taskDB : function(callback){// 创建 || 打开 数据库-任务
@@ -313,6 +335,7 @@
 		getCourseIdAll : DB.getCourseIdAll,
 		getTaskProgress : DB.getTaskProgress,
 		getCourseTaskProgress : DB.getCourseTaskProgress,
-		getTaskProgressNoSend : DB.getTaskProgressNoSend
+		getTaskProgressNoSend : DB.getTaskProgressNoSend,
+		getTasksProgressSupplyAll : DB.getTasksProgressSupplyAll
 	}
 })(window);
--- demo/script/saveTasksProgress.js
+++ demo/script/saveTasksProgress.js
@@ -5,21 +5,15 @@
 		user_memberId : '',
 		saveServerNum : 0,
 		saveDBNum : 0,
-		online : false,
 		init : function(){
-			var online = saveTasksProgress.isOnline();
-			if(online){
+			if(saveTasksProgress.online()){
 				saveTasksProgress.save();
 			}else{
 
 			}
 		},
-		isOnline : function(){
-			if(api.connectionType!='unknown' || api.connectionType!='none'){
-				return true;
-			}else{
-				return false;
-			}
+		online : function(){
+			return (api.connectionType!='unknown' || api.connectionType!='none');
 		},
 		save : function(courseId){
 			DB.getCourseIdAll(function(ret, err){
@@ -35,24 +29,27 @@
 			DB.getCourseTaskProgress(courseId,function(data){
 				// alert('nosend:::'+JSON.stringify(data));
 				if(data && data.length){
-					saveTasksProgress.maxProgress(courseId, data, function(updateDBData, updateServerData){
+
+					saveTasksProgress.getUpdateDBData(courseId, data, function(updateDBData, updateServerData){
 						// alert('updateDBData::'+JSON.stringify(updateDBData))
 						// alert('updateServerData::'+JSON.stringify(updateServerData))
 						
 						if(updateDBData && updateDBData.length){
 							saveTasksProgress.saveDB(updateDBData);
 						}
-						if(updateServerData && updateServerData.length){
-							saveTasksProgress.user_nickname = getstor('nickName');
-							saveTasksProgress.user_token = getstor('token');
-							saveTasksProgress.user_memberId = getstor('memberId');
-							saveTasksProgress.saveServer(updateServerData);
-						}
+						// if(updateServerData && updateServerData.length){
+						// 	saveTasksProgress.saveServer(updateServerData);
+						// }
+
 					});
+					DB.getTasksProgressSupplyAll(function(data){
+						saveTasksProgress.saveServer(data);
+					})
+					
 				}
 			})
 		},
-		maxProgress : function(courseId, data, callback){
+		getUpdateDBData : function(courseId, data, callback){
 			var updateTasksProgressDB = [];
 			var updateTasksProgressServer = [];
 			var tasksProgressDB = data;
@@ -62,41 +59,47 @@
 
 			//获取课程任务进度列表（new）tested
 	        var param = {
-	            'token': $api.getStorage('token'), //必须
+	            'token': getstor('token'), //必须
+	            'memberId' : getstor('memberId')
 	            'courseId': courseId //课程ID,必须
 	        };
-	        ajaxRequest('api/v2/study/getTasksProgress', 'get', param, function (ret, err) {//008.022 获取课程任务进度列表（new）tested，接口编号：008-022
+	        ajaxRequest('api/userAction/course/getTasksProgress/v1.0/', 'get', param, function (ret, err) {
+	        // ajaxRequest('api/v2/study/getTasksProgress', 'get', param, function (ret, err) {//008.022 获取课程任务进度列表（new）tested，接口编号：008-022
 	            if (err) {
 	                return false;
 	            } else if (ret && ret.state == 'success') {
 	                tasksProgressServer = ret.data;
 	                tasksProgressServerLength = ret.data.length;
 	                
-	                	for(var i=0;i<tasksProgressDBLength;i++){
-	                	    var different = 0;
-	                	    for(var j=0;j<tasksProgressServerLength;j++){
-	                	        if(tasksProgressDB[i].taskId==tasksProgressServer[j].taskId){
-	                	        	//createDate 比较创建时间
-	                	        	var createDateDB = parseInt(tasksProgressDB[i].createDate)
-	                	        	var createDateServer = parseInt(tasksProgressServer[j].createDate)
-	                	            if(createDateDB>createDateServer){
-	                	              updateTasksProgressServer.push(tasksProgressDB[i]);
-	                	            }else if(createDateDB<createDateServer){
-	                	            	updateTasksProgressDB.push(tasksProgressServer[j]);
-	                	            }
-	                	        }else{
-	                	            different ++;
-	                	        }
-	                	    }
-	                	    if(different >= tasksProgressServerLength){
-				                    updateTasksProgressServer.push(tasksProgressDB[i]);
-				                }
-	                	}
+	                	// for(var i=0;i<tasksProgressDBLength;i++){
+	                	//     var different = 0;
+	                	//     for(var j=0;j<tasksProgressServerLength;j++){
+	                	//         if(tasksProgressDB[i].taskId==tasksProgressServer[j].taskId){
+	                	//         	//createDate 比较创建时间
+	                	//         	var createDateDB = parseInt(tasksProgressDB[i].createDate)
+	                	//         	var createDateServer = parseInt(tasksProgressServer[j].createDate)
+	                	//             if(createDateDB>createDateServer){
+	                	//               updateTasksProgressServer.push(tasksProgressDB[i]);
+	                	//             }else if(createDateDB<createDateServer){
+	                	//             	updateTasksProgressDB.push(tasksProgressServer[j]);
+	                	//             }
+	                	//         }else{
+	                	//             different ++;
+	                	//         }
+	                	//     }
+	                	//     if(different >= tasksProgressServerLength){
+				              //       updateTasksProgressServer.push(tasksProgressDB[i]);
+				              //   }
+	                	// }
 	                for(var i=0;i<tasksProgressServerLength;i++){
 	                	var different = 0;
 	                	for(var j=0;j<tasksProgressDBLength;j++){
 	                		if(tasksProgressDB[j].taskId==tasksProgressServer[i].taskId){
-
+	                				var createDateDB = parseInt(tasksProgressDB[j].createDate)
+	                				var createDateServer = parseInt(tasksProgressServer[i].createDate)
+	                			    if(createDateDB<createDateServer){
+	                			    	updateTasksProgressDB.push(tasksProgressServer[i]);
+	                			    }
 	                		}else{
 	                			different ++;
 	                		}
@@ -124,23 +127,28 @@
 			}
 			var taskProgressData = data[saveTasksProgress.saveServerNum];
 			var post_param = {
-			    token : saveTasksProgress.user_token, //必须，用户token    144594636417159iPhoneCourse
-			    memberId : saveTasksProgress.user_memberId, //必须，用户id ff8080815065f95a01506627ad4c0007
-			    memberName : saveTasksProgress.user_nickname, //必须，用户昵称   zhangxiaoyu01
+			    token : taskProgressData.token, //必须，用户token    144594636417159iPhoneCourse
+			    memberId : taskProgressData.memberId, //必须，用户id ff8080815065f95a01506627ad4c0007
+			    memberName : taskProgressData.memberName, //必须，用户昵称   zhangxiaoyu01
+			    
+			    categoryId : taskProgressData.categoryId, //必须，证书id    ff808081473905e701475cd3c2080001
+			    categoryName : taskProgressData.categoryName,
+			    subjectId : taskProgressData.subjectId, //必须，科目id  ff808081473905e7014762542d940078
+			    subjectName : taskProgressData.subjectName,
+			    courseId : taskProgressData.courseId, //必须，课程id    ff808081486933e6014889882d9c0590
+			    courseName : taskProgressData.courseName, //必须，课程名称    courseName
+			    chapterId : taskProgressData.chapterId, //必须，章节id   chapterId
+			    chapterName : taskProgressData.chapterName, //必须，章节名称   chapterName
+			    taskId : taskProgressData.taskId, //必须，任务id    1
+			    taskName : taskProgressData.title, //必须，任务名称   taskName
 
 			    progress : taskProgressData.progress, //必须，当前进度值，视频为秒，试卷为题数量，文档为页码   5
-			    taskId : taskProgressData.taskId, //必须，任务id    1
-			    chapterId : taskProgressData.chapterId, //必须，章节id   chapterId
-			    courseId : taskProgressData.courseId, //必须，课程id    ff808081486933e6014889882d9c0590
-			    taskName : taskProgressData.title, //必须，任务名称   taskName
-			    chapterName : taskProgressData.chapterName, //必须，章节名称   chapterName
-			    courseName : taskProgressData.courseName, //必须，课程名称    courseName
 			    total : taskProgressData.total, //必须，任务总长度   48
-			    subjectId : taskProgressData.subjectId, //必须，科目id  ff808081473905e7014762542d940078
-			    categoryId : taskProgressData.categoryId, //必须，证书id    ff808081473905e701475cd3c2080001
-			    state : taskProgressData.state//必须，进度状态默认init，完成：complate    complate
+			    state : taskProgressData.state,//必须，进度状态默认init，完成：complate    complate
+			    createDate : taskProgressData.createDate,
+			    isSupply : 1
 			};
-			ajaxRequest('api/v2.1/chapter/taskProgress', 'post', post_param, function(ret, err) {//008.024保存任务进度日志（new）tested
+			ajaxRequest('api/v2.1/chapter/taskProgress', 'get', post_param, function(ret, err) {//008.024保存任务进度日志（new）tested
 			    if(err){
 			        api.toast({
 			            msg : err.msg,
@@ -159,7 +167,6 @@
 	}
 	window.saveTasksProgress = {
 		init : saveTasksProgress.init,
-		save : saveTasksProgress.save,
 		getCourseTaskProgress : saveTasksProgress.getCourseTaskProgress
 	}
 })(window);
